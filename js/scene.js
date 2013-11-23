b8e1104b5fc9b8e7dcd9ed759516d7cc1010689c
cubicledowns <cbcldowns@gmail.com>
2013-11-22 17:07:55 -0800

    Initial setup for blog code.

---
+++ js/scene.js
@@ -0,0 +1,83 @@
+var Demo = Demo || {};
+Demo.Scene = Demo.Scene || {};
+
+Demo.Scene = function (containerId) {
+
+  this.scene = new THREE.Scene();
+  this.projector = new THREE.Projector();
+  this.raycaster = new THREE.Raycaster();
+
+  this.renderer = new THREE.WebGLRenderer({antialias: true});
+
+  this.jqContainer = $('#' + containerId);
+  this.container = document.getElementById(containerId);
+  // an array of scene elements we're interested with regards to collisions
+  this.collisions = [];
+
+  this.cameras = new Demo.Cameras(this);
+  this.setup = new Demo.Scene.Setup({ context: this, cubes: 3});
+  this.stats = null;
+
+
+  this.theta = 0;
+  this.radius = 100;
+
+  this.init();
+
+};
+
+Demo.Scene.prototype = {
+
+  init: function () {
+    this.listeners();
+    this.statsSetup();
+
+  },
+
+  listeners: function () {
+    var me = this;
+    window.addEventListener('resize', me.onWindowResize, false);
+  },
+
+  onWindowResize: function (e) {
+    this.liveCam.aspect = this.jqContainer.width() / this.jqContainer.height();
+    this.liveCam.updateProjectionMatrix();
+
+    this.renderer.setSize(this.jqContainer.width(), this.jqContainer.height());
+  },
+
+  statsSetup: function () {
+    this.stats = new Stats();
+    this.stats.domElement.style.position = 'absolute';
+    this.stats.domElement.style.top = '0px';
+    this.container.appendChild( this.stats.domElement );
+  },
+
+  /**
+   * Animates the scene
+   */
+  animate: function () {
+    // NOTE: using the global variable "demo" instead of "this".
+      requestAnimationFrame(demo.animate);
+      demo.render();
+      demo.stats.update();
+  },
+
+  /**
+   * Renders the WebGL scene
+   */
+  render: function () {
+
+    this.theta += 0.1;
+
+    this.cameras.liveCam.position.x = this.radius * Math.sin(THREE.Math.degToRad(this.theta));
+    this.cameras.liveCam.position.y = this.radius * Math.sin(THREE.Math.degToRad(this.theta));
+    this.cameras.liveCam.position.z = this.radius * Math.cos(THREE.Math.degToRad(this.theta));
+    this.cameras.liveCam.lookAt(this.scene.position);
+
+    var temp = this.scene.position;
+
+    // NOTE: using the global variable "demo" instead of "this".
+    this.renderer.render(this.scene, demo.cameras.liveCam);
+  }
+};
---
+++ js/setup.js
@@ -0,0 +1,99 @@
+
+var Demo = Demo || {};
+Demo.Scene = Demo.Scene || {};
+
+
+/**
+ * @class Scene setup.  Most initialization of geometry and managers happen here.
+ */
+Demo.Scene.Setup = function (params) {
+
+  this.context = params.context;
+
+  this.numCubes = params.cubes || 1;
+
+  this.width = this.context.jqContainer.width();
+  this.height = this.context.jqContainer.height();
+
+  this.mesh = null;
+
+  this.init();
+};
+
+Demo.Scene.Setup.prototype = {
+
+  /**
+   * Initialize all of the THREE.JS framework and additional code.
+   */
+  init: function () {
+
+    this.setupRenderer();
+
+    this.lights();
+    this.createGeometry();
+  },
+
+  /**
+   * Setup the render inforamtion.
+   */
+  setupRenderer: function () {
+    this.context.renderer.setSize(this.width, this.height);
+    this.context.container.appendChild(this.context.renderer.domElement);
+  },
+
+  /**
+   * Add supporting geometry to the scene.  Axis helpers, stats, grid etc.
+   */
+  createGeometry: function () {
+    this.context.scene.add(new THREE.AxisHelper(10));
+    this.createCubes();
+  },
+
+  /**
+   *  Create cubes yo.
+   *
+   */
+  createCubes: function () {
+
+    var i,
+        mesh,
+        material,
+        color,
+        cube = new THREE.CubeGeometry(25, 25, 25);
+
+    for(i = 0; i < this.numCubes; i++) {
+
+      //thx Paul Irish et al.
+      color = new THREE.Color().setHex('0x' + ('000000' + Math.floor(Math.random()*16777215).toString(16)).slice(-6));
+      material = new THREE.MeshLambertMaterial({color: color});
+
+      this.mesh = new THREE.Mesh(cube, material);
+
+      this.mesh.position.x = Math.random() * 100 - 50;
+      this.mesh.position.y = Math.random() * 100 - 50;
+      this.mesh.position.z = Math.random() * 100 - 50;
+
+      this.context.collisions.push(this.mesh);
+
+      this.context.scene.add(this.mesh);
+    }
+
+  },
+
+  /**
+   * Add light(s) to the scene
+   */
+  lights: function () {
+
+    var dl,
+        rotate = 30 * Math.PI / 180;
+
+    this.context.scene.add(new THREE.AmbientLight(0x999999));
+
+    dl = new THREE.DirectionalLight(0xFFFFFF, 1);
+    dl.position.set(rotate,rotate,rotate);
+
+    this.context.scene.add(dl);
+  }
+};
+
