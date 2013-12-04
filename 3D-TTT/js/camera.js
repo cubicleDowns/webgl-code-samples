28c9359298e4454a71a8b46d1d1756e75478f4cc
cubicledowns <cbcldowns@gmail.com>
2013-12-03 17:25:05 -0800

    Adding initial 3D tic-tac-toe work

---
+++ 3D-TTT/js/camera.js
@@ -0,0 +1,47 @@
+var Demo = Demo || {};
+Demo.Cameras = Demo.Cameras || {};
+
+/**
+ * @namespace  Camera initialization
+ * @class Creates cameras for the scene.
+ */
+Demo.Cameras = function (context) {
+
+    this.context = context;
+
+    this.liveCam = null;
+    this.FOV = 70;
+    this.WIDTH = context.jqContainer.width();
+    this.HEIGHT = context.jqContainer.height();
+    this.ASPECT_RATIO = this.WIDTH / this.HEIGHT;
+    this.NEAR_PLANE = 1;
+    this.FAR_PLANE = 100000;
+
+    this.controls = null;
+
+    this.init();
+};
+
+Demo.Cameras.prototype = {
+
+    /**
+     * Initialize the camera object and create default cameras.
+     */
+    init: function () {
+        this.initPerspective();
+        this.initControls();
+    },
+
+    initControls: function () {
+        this.controls = new THREE.OrbitControls( this.liveCam, document.getElementById('ray-intersection') );
+    },
+
+    /**
+     * Initialize the perspective camera.
+     */
+    initPerspective: function () {
+        this.liveCam = new THREE.PerspectiveCamera(this.FOV, this.ASPECT_RATIO, this.NEAR_PLANE, this.FAR_PLANE);
+        this.liveCam.position.z = 300;
+    }
+
+};
---
+++ 3D-TTT/js/scene.js
@@ -0,0 +1,83 @@
+var Demo = Demo || {};
+Demo.Scene = Demo.Scene || {};
+
+Demo.Scene = function (containerId) {
+
+  this.scene = new THREE.Scene();
+  this.projector = new THREE.Projector();
+  this.renderer = new THREE.WebGLRenderer({antialias: true});
+
+  this.jqContainer = $('#' + containerId);
+  this.container = document.getElementById(containerId);
+  // an array of scene elements we're interested with regards to collisions
+  this.collisions = [];
+
+  this.cameras = new Demo.Cameras(this);
+  this.setup = new Demo.Scene.Setup({ context: this, cubes: 3});
+  this.stats = null;
+
+  this.rotateCamera = false;
+
+  this.theta = 0;
+  this.radius = 100;
+
+  this.init();
+
+};
+
+Demo.Scene.prototype = {
+
+  init: function () {
+    this.listeners();
+    this.statsSetup();
+
+  },
+
+  listeners: function () {
+    var me = this;
+    window.addEventListener('resize', me.onWindowResize, false);
+  },
+
+  onWindowResize: function (e) {
+    this.liveCam.aspect = this.jqContainer.width() / this.jqContainer.height();
+    this.liveCam.updateProjectionMatrix();
+
+    this.renderer.setSize(this.jqContainer.width(), this.jqContainer.height());
+  },
+
+  statsSetup: function () {
+    this.stats = new Stats();
+    this.stats.domElement.style.position = 'absolute';
+    this.stats.domElement.style.top = '0px';
+    this.container.appendChild( this.stats.domElement );
+  },
+
+  /**
+   * Animates the scene
+   */
+  animate: function () {
+    // NOTE: using the global variable "demo" instead of "this".
+      requestAnimationFrame(_demo.animate);
+      _demo.render();
+      _demo.stats.update();
+      _demo.cameras.controls.update();
+  },
+
+  /**
+   * Renders the WebGL scene
+   */
+  render: function () {
+
+    if(this.rotateCamera){
+      this.theta += 0.3;
+
+      this.cameras.liveCam.position.x = this.radius * Math.sin(THREE.Math.degToRad(this.theta));
+      this.cameras.liveCam.position.y = this.radius * Math.sin(THREE.Math.degToRad(this.theta));
+      this.cameras.liveCam.position.z = this.radius * Math.cos(THREE.Math.degToRad(this.theta));
+      this.cameras.liveCam.lookAt(this.scene.position);
+    }
+
+    // NOTE: using the global variable "demo" instead of "this".
+    this.renderer.render(this.scene, _demo.cameras.liveCam);
+  }
+};
